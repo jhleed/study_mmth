b83caae554afa2d4f4af425daad8b08b629d3e02
jongholee <jhleed3@gmail.com>
2018-05-23 21:35:45 +0900

    테스트 추가

--- src/test/java/step1/CustomerTest.java
+++ src/test/java/step1/CustomerTest.java
@@ -6,10 +6,20 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
+    private Customer customer;
+
     @Test
-    public void statement_이름_null() {
-        Customer customer = new Customer(null);
+    public void 생성자_null() {
+        customer = new Customer(null);
         assertEquals("Rental Record for null\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter pointers", customer.statement());
+    }
+
+    @Test
+    public void 생성자_이름_설정() {
+        customer = new Customer("종호");
+        assertEquals("Rental Record for 종호\n" +
                 "Amount owed is 0.0\n" +
                 "You earned 0 frequent renter pointers", customer.statement());
     }
