7ade77d32aebf74d4d2dfa601ad3ca0c0cd429ce
jongholee <jhleed3@gmail.com>
2018-05-24 20:54:24 +0900

    htmlStatement 추가

--- src/main/java/step1/Customer.java
+++ src/main/java/step1/Customer.java
@@ -24,7 +24,6 @@ class Customer {
     ;
 
     public String statement() {
-
         String result = "Rental Record for " + getName() + "\n";
         for (Rental rental : _rentals) {
             result += "\t" + String.valueOf(rental.getCharge()) + "(" + rental.getMovie().getTitle() + ")" + "\n";
@@ -32,6 +31,18 @@ class Customer {
 
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter pointers";
+
+        return result;
+    }
+
+    public String htmlStatement() {
+        String result = "<h1>Rental Record for " + getName() + "</h1>\n";
+        for (Rental rental : _rentals) {
+            result += "\t" + String.valueOf(rental.getCharge()) + "(" + rental.getMovie().getTitle() + ")" + "\n";
+        }
+
+        result += "<p>Amount owed is " + String.valueOf(getTotalCharge()) + "</p>\n";
+        result += "<p>You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter pointers</p>";
 
         return result;
     }
